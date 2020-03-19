f8ad0103de9cb40a8f5a89be494bf91f827a42a9
Aashish Bansal <aashish22bansal@gmail.com>
2020-03-19 12:39:59 +0530

    Add files via upload

---
+++ Basic Java Programs/Date_and_Time.java
@@ -0,0 +1,18 @@
+import sun.util.resources.cldr.ln.CalendarData_ln_CD;
+
+import java.util.*;
+public class Date_and_Time
+{
+    public static void main(String args[])
+    {
+        int second,day,month,year,minute,hour;
+        GregorianCalendar date=new GregorianCalendar();
+        day=date.get(Calendar.DAY_OF_MONTH);
+        month=date.get(Calendar.MONTH);
+        year=date.get(Calendar.YEAR);
+        second=date.get(Calendar.SECOND);
+        minute=date.get(Calendar.MINUTE);
+        hour=date.get(Calendar.HOUR);
+        System.out.print("Current Date: "+day+"/"+month+"/"+year+"\nCurrent Time: "+hour+":"+minute+":"+second);
+    }
+}
